d56a5ae5dafa6ea8108fe60f66f1d4c496efd182
Benoit D'André <dandre.benoit@outlook.com>
2024-10-17 19:20:10 -0400

    Setup typescript and vue

---
+++ resources/vue/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h1>The Bid Calculation Tool</h1>
+        <div>
+        </div>
+    </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
+import path from 'path';
 import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue";
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/vue/app.ts'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+		    "@": path.resolve(__dirname, './resources/vue'),
+        }
+    }
 });
