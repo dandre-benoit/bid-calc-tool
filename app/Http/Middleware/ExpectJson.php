9d565abf5baa1064ef7c1cd5ce01e08310086233
Benoit D'André <dandre.benoit@outlook.com>
2024-10-17 17:46:03 -0400

    Add middleware that expect JSON for group api

--- app/Http/Middleware/ExpectJson.php
+++ app/Http/Middleware/ExpectJson.php
@@ -10,7 +10,7 @@ class ExpectJson
     public function handle(Request $request, Closure $next)
     {
         $request->headers->set('Accept', 'application/json');
-        
+
         return $next($request);
     }
 }
